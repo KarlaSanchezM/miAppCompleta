856cc7586bd6375661f8ca7e2c16bde0e6fe9453
KarlaSanchezM <karlamouret1513@gmail.com>
2020-04-27 01:26:27 -0500

    miAppCompleta

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 'D3c4Sm2jw7',
+        password: '2A07U9eVbN',
+        database: 'D3c4Sm2jw7'
+    }
+
+};
